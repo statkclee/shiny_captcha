93ed3e74b2b8ba1af35e9113e005c8ec7cdcc28d
Kwangchun Lee <kwangchun.lee.7@gmail.com>
2020-07-02 08:44:27 +0900

    main.R added

--- main.R
+++ main.R
@@ -1,11 +1,15 @@
 # 모형 파이프라인
 
-source("00-library.R")
+run_main <- function() {
+    source("00-library.R")
+    
+    source("01-ingest.R")
+    
+    source("02-preprocess.R")
+    
+    source("03-training.R")
+    
+    source("04-evaluate.R")
+}
 
-source("01-ingest.R")
-
-source("02-preprocess.R")
-
-source("03-training.R")
-
-source("04-evaluate.R")
+run_main()
